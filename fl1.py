5d2ebc6cb0176748703a36905cec63fa41cf83c1
CaelCoruscare <caelsumner@gmail.com>
2018-02-11 01:51:56 -0500

    Fixed the Python JSON accept and response

--- fl1.py
+++ fl1.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request,render_template
+from flask import Flask, jsonify, request,render_template, make_response
 from navigation import time_dist
 from jericho import * 
 
@@ -7,11 +7,28 @@ app = Flask(__name__)
 
 @app.route("/_get_time_dist")
 def _get_time_dist():
+    print("--+--+--+--")
     Iorigin = request.args.get('origin',0)
     Idestination = request.args.get('destination',0)
     such = time_dist(Iorigin,Idestination)
     result = str(such[0]) + str(such[1])
     return jsonify(result=result)
+
+@app.route("/_graph_",methods=['POST','GET'])
+def _graph_():
+    print("--------")
+    #print(request.method)
+    print(request.form.to_dict())
+    print("--------")
+    if(request.method == 'GET'):
+        result = "help"
+    return jsonify(statusText=result)
+
+@app.route("/caeltest")
+def caeltest():
+    resp = make_response(jsonify('hello'))
+    resp.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
+    return resp
 
 @app.route("/")
 def root():
