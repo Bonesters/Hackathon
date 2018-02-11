0eed7c1be544dac96a9ed6c446fa9a2f81d19fcc
heyday665 <jackdacey@landmark.edu>
2018-02-10 22:31:48 -0500

    Update jericho.py
    improvements for jericho,

--- jericho.py
+++ jericho.py
@@ -9,62 +9,107 @@ database = "GPSDATA"
 connection = pymssql.connect(server, username, password, database)
 cursor = connection.cursor()
 
-#def getAllVehicles():
-#    cursor.execute(
-#        """
-#        SELECT * FROM Vehicle;
-#        """
-#    )    
-#    data = cursor.fetchall()
-#    print(data)
-#    print()
-#    return json.dumps(data)
-
 def timePerTruckPerDay():
     cursor.execute(
         """
-        SELECT CONVERT(varchar,CONVERT(datetime,(MAX(CONVERT(float,StartTime))-MIN(CONVERT(float,StartTime))))), VehicleID 
+        SELECT CONVERT(varchar,CONVERT(datetime,(MAX(CONVERT(float,VehicleEvent.StartTime))-MIN(CONVERT(float,VehicleEvent.StartTime))))),
+        CONVERT(varchar,MIN(VehicleEvent.StartTime)),Vehicle.DisplayName
         FROM VehicleEvent 
-        GROUP BY VehicleID,DATEPART(year,StartTime),DATEPART(month,StartTime),DATEPART(day,StartTime);
+        INNER JOIN Vehicle ON VehicleEvent.VehicleID=Vehicle.VehicleID
+        GROUP BY Vehicle.DisplayName,DATEPART(year,StartTime),DATEPART(month,StartTime),DATEPART(day,StartTime)
+        ORDER BY DATEPART(year,StartTime),DATEPART(month,StartTime),DATEPART(day,StartTime),CONVERT(int,Vehicle.DisplayName);
         """
     )
 
     data = cursor.fetchall()
+
+    newData = list(data)
+
+    for x in range(0,11):
+        newData[x] = list(data[x])
+        newData[x][0] = list(data[x][0])
+        temp = str(data[x][0])[13:-2]
+        newData[x][0] = temp
+
+        newData[x][1] = list(data[x][1])
+        temp = str(data[x][1])[0:-8]
+        newData[x][1] = temp
+
+    jsonMemes = json.dumps(newData)
+    print(jsonMemes)
+    return jsonMemes
+
+def timePerTruck():
+    cursor.execute(
+        """
+        SELECT CONVERT(varchar,CONVERT(datetime,(MAX(CONVERT(float,VehicleEvent.StartTime))-MIN(CONVERT(float,VehicleEvent.StartTime))))),Vehicle.DisplayName 
+        FROM VehicleEvent 
+        INNER JOIN Vehicle ON VehicleEvent.VehicleID=Vehicle.VehicleID
+        GROUP BY Vehicle.DisplayName
+        ORDER BY CONVERT(int,Vehicle.DisplayName);
+        """
+    )
+
+    data = cursor.fetchall()
+    dlen = len(data)
+    print(dlen)
+    print(data[0])
+    print(data[1])
+    print(data[2])
+    print(data[3])
+    print(data[4])
+    print(data[5])
+    print(data[6])
+    print(data[7])
+    print(data[8])
+    print(data[9])
+    print(data[10])
+    print(data[11])
+    print(data[12])
     return json.dumps(data)
 
 def timePerDay():
     cursor.execute(
         """
-        SELECT CONVERT(varchar,CONVERT(datetime,(MAX(CONVERT(float,StartTime))-MIN(CONVERT(float,StartTime)))))
+        SELECT CONVERT(varchar,CONVERT(datetime,(MAX(CONVERT(float,StartTime))-MIN(CONVERT(float,StartTime))))),CONVERT(varchar,MIN(StartTime))
         FROM VehicleEvent 
-        GROUP BY DATEPART(year,StartTime),DATEPART(month,StartTime),DATEPART(day,StartTime);
+        GROUP BY DATEPART(year,StartTime),DATEPART(month,StartTime),DATEPART(day,StartTime)
+        ORDER BY DATEPART(year,StartTime),DATEPART(month,StartTime),DATEPART(day,StartTime);
         """
     )
 
     data = cursor.fetchall()
+    dlen = len(data)
+    print(dlen)
+    print(data[0])
     return json.dumps(data)
 
+#Returns nothing
 def getRecentLocations():
     cursor.execute(
         """
-        SELECT VehicleID,Latitude,Longitude,StartTime,Heading,EventTypeID,Location 
+        SELECT Vehicle.DisplayName,Latitude,Longitude,CONVERT(varchar,VehicleEvent.StartTime),Heading,EventTypeID,Location 
         FROM VehicleEvent 
-        WHERE DAY(StartTime)=DAY(CURRENT_TIMESTAMP) AND MONTH(StartTime)=MONTH(CURRENT_TIMESTAMP) AND YEAR(StartTime)=YEAR(CURRENT_TIMESTAMP)
-        ORDER BY StartTime;
+        INNER JOIN Vehicle ON VehicleEvent.VehicleID=Vehicle.VehicleID
+        WHERE DAY(VehicleEvent.StartTime)=DAY(CURRENT_TIMESTAMP) AND MONTH(VehicleEvent.StartTime)=MONTH(CURRENT_TIMESTAMP) AND YEAR(VehicleEvent.StartTime)=YEAR(CURRENT_TIMESTAMP)
+        ORDER BY VehicleEvent.StartTime;
         """
     )
 
     data = cursor.fetchall()
+    dlen = len(data)
+    print(dlen)
     return json.dumps(data)
 
 def main():
-    print(getAllVehicles())
+    timePerTruckPerDay()
     print("\n")
-    print(timePerTruckPerDay())
-    print("\n")
-    print(timePerDay())
-    print("\n")
-    print(getRecentLocations())
+    #timePerDay()
+    #print("\n")
+    #timePerTruck()
+    #print("\n")
+    #getRecentLocations()
+    #print("\n")
 
 if (__name__ == "__main__"):
     main()
