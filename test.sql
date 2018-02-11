0e875bf308ee18846ca76d572505357ef81b1e08
Bonesters <bones03052@gmail.com>
2018-02-11 01:54:21 -0500

    added sql to get data for dates in range.

--- test.sql
+++ test.sql
@@ -42,13 +42,20 @@ SELECT CONVERT(varchar,CONVERT(datetime,(MAX(CONVERT(float,VehicleEvent.StartTim
 CONVERT(varchar,MIN(VehicleEvent.StartTime)),Vehicle.DisplayName
 FROM VehicleEvent 
 INNER JOIN Vehicle ON VehicleEvent.VehicleID=Vehicle.VehicleID
-WHERE StartTime BETWEEN CONVERT(datetime,'mm/dd/yyyy',1) AND CONVERT(datetime,'mm/dd/yyyy',1)
+WHERE StartTime BETWEEN CONVERT(datetime,'mm/dd/yyyy',1) AND DATEADD(day,1,CONVERT(datetime,'mm/dd/yyyy',1))
 GROUP BY Vehicle.DisplayName,DATEPART(year,StartTime),DATEPART(month,StartTime),DATEPART(day,StartTime)
 ORDER BY DATEPART(year,StartTime),DATEPART(month,StartTime),DATEPART(day,StartTime),CONVERT(int,Vehicle.DisplayName);
 
 /*---time per day in range---*/
 SELECT CONVERT(varchar,CONVERT(datetime,(MAX(CONVERT(float,StartTime))-MIN(CONVERT(float,StartTime))))),CONVERT(varchar,MIN(StartTime))
 FROM VehicleEvent 
-WHERE StartTime BETWEEN CONVERT(datetime,'mm/dd/yyyy',1) AND CONVERT(datetime,'mm/dd/yyyy',1)
+WHERE StartTime BETWEEN CONVERT(datetime,'mm/dd/yyyy',1) AND DATEADD(day,1,CONVERT(datetime,'mm/dd/yyyy',1))
 GROUP BY DATEPART(year,StartTime),DATEPART(month,StartTime),DATEPART(day,StartTime)
 ORDER BY DATEPART(year,StartTime),DATEPART(month,StartTime),DATEPART(day,StartTime);
+
+/*---all events from date range---*/
+SELECT Vehicle.DisplayName,Latitude,Longitude,CONVERT(varchar,VehicleEvent.StartTime),Heading,EventTypeID,Location 
+FROM VehicleEvent 
+INNER JOIN Vehicle ON VehicleEvent.VehicleID=Vehicle.VehicleID
+WHERE VehicleEvent.StartTime BETWEEN CONVERT(datetime,'mm/dd/yyyy',1) AND DATEADD(day,1,CONVERT(datetime,'mm/dd/yyyy',1))
+ORDER BY VehicleEvent.StartTime;
