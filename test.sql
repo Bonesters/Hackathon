8e47634c9fae3cf1ed91460c4cdbbc518f0d789f
Bonesters <bones03052@gmail.com>
2018-02-10 21:52:50 -0500

    sql now returns display name instead of ID.

--- test.sql
+++ test.sql
@@ -1,29 +1,37 @@
 USE GPSDATA;
 
+/*
 SELECT * FROM EventType;
 SELECT * FROM Vehicle;
 SELECT * FROM VehicleEvent;
+*/
 
 /*---time per truck, per day---*/
-SELECT CONVERT(varchar,CONVERT(datetime,(MAX(CONVERT(float,StartTime))-MIN(CONVERT(float,StartTime))))), VehicleID 
+SELECT CONVERT(varchar,CONVERT(datetime,(MAX(CONVERT(float,VehicleEvent.StartTime))-MIN(CONVERT(float,VehicleEvent.StartTime))))),
+CONVERT(varchar,MIN(VehicleEvent.StartTime)),Vehicle.DisplayName
 FROM VehicleEvent 
-GROUP BY VehicleID,DATEPART(year,StartTime),DATEPART(month,StartTime),DATEPART(day,StartTime);
+INNER JOIN Vehicle ON VehicleEvent.VehicleID=Vehicle.VehicleID
+GROUP BY Vehicle.DisplayName,DATEPART(year,StartTime),DATEPART(month,StartTime),DATEPART(day,StartTime)
+ORDER BY DATEPART(year,StartTime),DATEPART(month,StartTime),DATEPART(day,StartTime),Vehicle.DisplayName;
 
 /*---time per truck---*/
-SELECT CONVERT(varchar,CONVERT(datetime,(MAX(CONVERT(float,StartTime))-MIN(CONVERT(float,StartTime))))), VehicleID 
+SELECT CONVERT(varchar,CONVERT(datetime,(MAX(CONVERT(float,VehicleEvent.StartTime))-MIN(CONVERT(float,VehicleEvent.StartTime))))),Vehicle.DisplayName 
 FROM VehicleEvent 
-GROUP BY VehicleID;
+INNER JOIN Vehicle ON VehicleEvent.VehicleID=Vehicle.VehicleID
+GROUP BY Vehicle.DisplayName;
 
 /*---time per day---*/
-SELECT CONVERT(varchar,CONVERT(datetime,(MAX(CONVERT(float,StartTime))-MIN(CONVERT(float,StartTime)))))
+SELECT CONVERT(varchar,CONVERT(datetime,(MAX(CONVERT(float,StartTime))-MIN(CONVERT(float,StartTime))))),CONVERT(varchar,MIN(StartTime))
 FROM VehicleEvent 
-GROUP BY DATEPART(year,StartTime),DATEPART(month,StartTime),DATEPART(day,StartTime);
+GROUP BY DATEPART(year,StartTime),DATEPART(month,StartTime),DATEPART(day,StartTime)
+ORDER BY DATEPART(year,StartTime),DATEPART(month,StartTime),DATEPART(day,StartTime);
 
 /*---all events from today---*/
-SELECT VehicleID,Latitude,Longitude,CONVERT(varchar,StartTime),Heading,EventTypeID,Location 
+SELECT Vehicle.DisplayName,Latitude,Longitude,CONVERT(varchar,VehicleEvent.StartTime),Heading,EventTypeID,Location 
 FROM VehicleEvent 
-WHERE DAY(StartTime)=DAY(CURRENT_TIMESTAMP) AND MONTH(StartTime)=MONTH(CURRENT_TIMESTAMP) AND YEAR(StartTime)=YEAR(CURRENT_TIMESTAMP)
-ORDER BY StartTime;
+INNER JOIN Vehicle ON VehicleEvent.VehicleID=Vehicle.VehicleID
+WHERE DAY(VehicleEvent.StartTime)=DAY(CURRENT_TIMESTAMP) AND MONTH(VehicleEvent.StartTime)=MONTH(CURRENT_TIMESTAMP) AND YEAR(VehicleEvent.StartTime)=YEAR(CURRENT_TIMESTAMP)
+ORDER BY VehicleEvent.StartTime;
 
 /*---current time---*/
 SELECT CURRENT_TIMESTAMP;
